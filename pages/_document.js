37009f61bb4bf063c8481f916ce6cdf66c7496a3
Nenad <nenadmarinkovic@protonmail.com>
2020-11-25 10:11:36 +0100

    initial optimized load time

--- pages/_document.js
+++ pages/_document.js
@@ -1,33 +1,30 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
-class WebSite extends Document {
+export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
 
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <meta charSet="UTF-8" />
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-          <meta
-            name="description"
-            content="Flickschuh Wien"
-          />
-          <meta name="author" content="Nenad Marinković" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
   }
 }
-
-export default WebSite;
